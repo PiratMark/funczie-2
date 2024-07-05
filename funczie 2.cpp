d13717f52463a817c6120c69ef21d49168ff67eb
Студент <@WIN-6S9RN55QEMH>
2024-07-05 16:06:49 +0500

    Добавьте файлы проекта.

---
+++ funczie 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+	double average(int arr[], int len)
+	{
+		double sum = 0;
+		for (int i = 0; i < len; i++)
+		{
+			sum += arr[i];
+		}
+		return sum / len;
+	}
+
+	int main()
+	{
+		int arr[] = { 1, 2, 3, 4, 5 };
+		int len = sizeof(arr) / sizeof(arr[0]);
+		cout << average(arr, len) << endl;
+		return 0;
+	}
+
+
